298532ccccbcd22d511adf4b270c6e4294eeb3f5
Chen <baochen95@gmail.com>
2018-11-13 11:06:30 +0800

    Accuracy 81.9%
    Improve the formular of the prior probability. The accuracy increased to 81.9%.

--- NBM/main_test.py
+++ NBM/main_test.py
@@ -5,56 +5,67 @@ def takeSecond(elem):
     return elem[1]
 
 f1=open("test_processedDoc.txt",encoding='ISO-8859-1')
-processedTest=f1.readlines() # the processed document that were split in to words
+processedTest=f1.readlines() # the processed testing document that were split in to words
 
 f2=open("cateProbability.txt",encoding='ISO-8859-1')
-cateProbability=f2.readlines() # the probability of each category
+cateProbability=f2.readlines() # the prior probability of each category
 newCate=[]
 for cate in cateProbability:
     newCate.append(float(cate.strip('\n')))
 cateProbability=newCate
 
 f3=open("wordCount.txt",encoding='ISO-8859-1')
-wordCount=f3.readlines() # the number of words in each document
+wordCount=f3.readlines() # the number of words in each category
 newcount=[]
 for count in wordCount:
     newcount.append(int(count.strip("\n")))
 wordCount=newcount
 
-filePath="E:/Coding/Data Mining/NBM/probability"
-files=os.listdir(filePath) # the probability of each word in each document of the training data
-
-probList=[]
-for file in files:
-    f4=open(filePath+"/"+file,encoding='ISO-8859-1')
+#################################
+filePath="D:/Coding/Data Mining/NBM/probability"
+dictionary={}
+probList=[] # the number of each word in each category of the training data. (word,categoryFrequency)
+for i in range(20):
+    f4=open(filePath+"/probability_"+str(i)+".txt",encoding='ISO-8859-1')
     probabilities=f4.readlines()
     docProb={}
     for probability in probabilities:
         probability=probability.strip("\n").split()
+        if probability[0] not in dictionary:
+            dictionary[probability[0]]=1
+            #print(probability[0])
         docProb[probability[0]]=float(probability[1])
     probList.append(docProb)
     f4.close()
-
+#print(len(dictionary))
+#################################
+classifiedCate=[]
+initialCate=[]
 f5=open("test_result.txt","w")
 for document in processedTest:
     document=document.strip("\n").split()
+    #print(document)
     documentProb=[]
+    initialCate.append(int(document[0]))
     for i in range(len(probList)):
         P_doc = 0
         for word in document[1:]:
             if word in probList[i]:
-                #print(probList[i][word])
-                #print((probList[i][word]+1)/(wordCount[i]+len(probList[i])))
-                #print(math.log((probList[i][word]+1)/(wordCount[i]+len(probList[i]))))
-
-                P_doc+=math.log((probList[i][word]+1)/(wordCount[i]+len(probList[i])))
+                P_doc+=math.log((probList[i][word]+1)/(wordCount[i]+len(dictionary)))
             else:
-                P_doc+=math.log(1/len(probList[i]))
+                P_doc+=math.log(1/(wordCount[i]+len(dictionary)))
             #print(P_doc)
         #print("HELLO")
         P_doc=P_doc+math.log(cateProbability[i])
         documentProb.append((i,P_doc))
     documentProb.sort(key=takeSecond,reverse=True)
-    print(documentProb[0][0])
+    classifiedCate.append(documentProb[0][0])
+    #print(documentProb[0][0])
     f5.write("%d\n" % documentProb[0][0])
 f5.close()
+
+accurate=0
+for i in range(len(initialCate)):
+    if initialCate[i]==classifiedCate[i]:
+        accurate+=1
+print(accurate/len(initialCate))
--- NBM/process.py
+++ NBM/process.py
@@ -2,7 +2,6 @@ import os
 import nltk
 import string
 import nltk.data
-import math
 
 from collections import Counter
 
@@ -38,8 +37,7 @@ def preProcessing(document):
             if (char not in string.punctuation) and (char not in string.digits):
                 word+=char
         lowerWord=word
-        #lowerWord=lowerWord.translate(string.punctuation)
-        #lowerWord=lowerWord.translate(string.digits)
+
         #print(lowerWord)
         if len(lowerWord)==0: #the word is deleted because it only contains punctuation or numbers
             continue
@@ -47,28 +45,13 @@ def preProcessing(document):
             lowerWord = lemma.lemmatize(lowerWord)  # stemming
         if lowerWord in stop: # delete stop words
             continue
+        elif len(lowerWord)<2: # delete words whose length is lower than 2
+            continue
         else:
             processedDocument.append(lowerWord)
     return processedDocument
 
-# create a dictionary
-def createDic(fileList):
-    wordDic={}
-    for file in fileList:
-        for document in file:
-            words=Counter(document[1:len(document)])
-            for word in words:
-                if word not in wordDic:
-                    wordDic[word]=1
-                else:
-                    wordDic[word]+=1
-    # filter those words whose document frequency is lower than 6
-    for word in list(wordDic.keys()):
-        if wordDic[word] <= 15:
-            del wordDic[word]
-    return wordDic
-
-def docProcess(path,fileList,tfDocumentList,flag):
+def docProcess(path,fileList,flag):
     files = os.listdir(path)  # obtain all file names under path [atl.atheism,comp.graphics,...]
     documents = []
     document_count=0
@@ -82,7 +65,7 @@ def docProcess(path,fileList,tfDocumentList,flag):
             documentRange=range(countTraining) #training
         else:
             documentRange=range(countTraining,len(documents))
-        #f=open("processedDoc_" + str(category_index)+ ".txt", "w", encoding='ISO-8859-1')
+
         for i in documentRange:
             document_count+=1
             currentDoc = open(path + "/" + file+"/"+documents[i],encoding='ISO-8859-1')
@@ -103,36 +86,12 @@ def docProcess(path,fileList,tfDocumentList,flag):
             documentLength = len(processedDocument) #number of words in one document
             count=Counter(processedDocument) #count the number of each word
             #print(count.most_common(10))
-            tf={}
-            for word in processedDocument:
-                if word not in tf:
-                    currentWordFre = count[word]
-                    tf[word]=currentWordFre/documentLength #tf value of appeared word in one single document
-                else:
-                    continue
-            #print(wordFrequency)
-            #print(tf)
+
             processedDocument=[category_index]+processedDocument
-            #print(processedDocument)
+
             documentList.append(processedDocument)
-            tfDocumentList.append(tf)
-            '''
-            for word in processedDocument:
-                f.write('%s ' % (word))
-            f.write("\n")
-            '''
+
         fileList.append(documentList)
         category_index+=1
-       # f.close()
 
     return document_count
-
-def calIDF(document_count,dictionary):
-    # computing idf value
-    idf = {}
-    #print(document_count)
-    #print(dictionary)
-    for word in dictionary:
-        idf[word] = math.log(abs(document_count) / (1 + dictionary[word]))
-
-    return idf
